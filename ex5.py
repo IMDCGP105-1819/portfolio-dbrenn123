33e8a5bdd33d4abbcb074ff86ede70ddfc75f8fb
Daniel <d.brennan@uos.ac.uk>
2018-10-07 21:11:39 +0100

    Replace '0' with the text 'no more' in printed lines.

--- ex5.py
+++ ex5.py
@@ -1,4 +1,5 @@
 # Bottle song
+ZERO_TEXT = "no more"
 # Loop 0-99
 for beers_drank in range(0, 100, 1):
     # Print: verse of bottle song with correct Number
@@ -6,10 +7,10 @@ for beers_drank in range(0, 100, 1):
 
     # Line 2
     if(beers_drank != 0):
-        print(f"Take one down, pass it around, {beers} bottles of beer on the wall.")
+        print(f"Take one down, pass it around, {beers or ZERO_TEXT} bottles of beer on the wall.")
 
     # Line 1
-    print(f"{beers} bottles of beer on the wall, {beers} bottles of beer...")
+    print(f"{beers or ZERO_TEXT.capitalize()} bottles of beer on the wall, {beers or ZERO_TEXT} bottles of beer...")
 
     # On final loop print: ending verse
     if(beers == 0):
