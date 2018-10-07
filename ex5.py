08ff776ba813795208645e6b2176f106da07c9e4
Daniel <d.brennan@uos.ac.uk>
2018-10-07 21:21:16 +0100

    Get loop count and final verse text from variable.

--- ex5.py
+++ ex5.py
@@ -1,9 +1,8 @@
-# Bottle song
+MAX_BEERS = 99
 ZERO_TEXT = "no more"
 # Loop 0-99
-for beers_drank in range(0, 100, 1):
-    # Print: verse of bottle song with correct Number
-    beers = 100 - beers_drank - 1
+for beers_drank in range(0, MAX_BEERS + 1, 1):
+    beers = MAX_BEERS - beers_drank # Number of bottles on wall
 
     # Line 2
     if(beers_drank != 0):
@@ -14,4 +13,4 @@ for beers_drank in range(0, 100, 1):
 
     # On final loop print: ending verse
     if(beers == 0):
-        print(f"Go to the store and buy some more, 99 bottles of beer on the wall")
+        print(f"Go to the store and buy some more, {MAX_BEERS} bottles of beer on the wall")
