9333ee395e9eca5b4ec7d0e790be636a861a79a1
Daniel <d.brennan@uos.ac.uk>
2018-10-08 19:34:05 +0100

    Calculate monthly savings and deposit

--- ex7.py
+++ ex7.py
@@ -1,6 +1,12 @@
 ## House deposit save time calculator
+DEPOSIT_RATE = 0.2
+SAVINGS_INTEREST_RATE = 0.04
 
 # Get required data from user
 total_cost = float(input("Enter house price (£#.##): £"))
 salary = float(input("Enter your annual salary (£#.##): £"))
 saved_portion = float(input("Enter amount to be saved: %")) / 100
+
+# Calculate required values
+savings_target = total_cost * DEPOSIT_RATE
+monthly_savings = salary * saved_portion / 12
