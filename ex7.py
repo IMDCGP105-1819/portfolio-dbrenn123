74fbbdd23863eff10440c79377a2b284a717bf67
Daniel <d.brennan@uos.ac.uk>
2018-10-08 19:19:09 +0100

    Get data required for calculations from user input

---
+++ ex7.py
@@ -0,0 +1,6 @@
+## House deposit save time calculator
+
+# Get required data from user
+total_cost = float(input("Enter house price (£#.##): £"))
+salary = float(input("Enter your annual salary (£#.##): £"))
+saved_portion = float(input("Enter amount to be saved: %")) / 100
