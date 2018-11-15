780ba2dd69dbf33a9af18b44082aeba538449fb7
Daniel <d.brennan@uos.ac.uk>
2018-11-15 19:36:29 +0000

    Add parse methods

--- fraction.py
+++ fraction.py
@@ -3,9 +3,10 @@ class Fraction(object):
         self._num = numerator
         self._denom = denominator
 
-    def _common_factor(*args, **kwargs):
+    @classmethod
+    def _common_factor(cls, *args, **kwargs):
         if args:
-            return Fraction._common_factor(*args[1:], factor=kwargs.get('factor', 1) * abs(args[0]))
+            return cls._common_factor(*args[1:], factor=kwargs.get('factor', 1) * abs(args[0]))
         else:
             return kwargs.get('factor')
 
@@ -15,9 +16,22 @@ class Fraction(object):
         # Reduce until sum of numerators cannot be reduced further
         ...
 
+    def __repr__(self):
+        return f"Fraction({self._num!r}, {self._denom!r})"
+
+    def __str__(self):
+        return f"{self._num}/{self._denom}"
+
+    def __int__(self):
+        return self._num // self._denom
+
+    def __float__(self):
+        return self._num / self._denom
+
+    def __eq__(self, other):
+        ...
+
     def __add__(self, other):
-        # Get common denominator
-        # Add numerator
         ...
 
     def __sub__(self, other):
@@ -35,16 +49,46 @@ class Fraction(object):
         # Multiply
         ...
 
-    def __str__(self):
-        return f"{self._num}/{self._denom}"
-
 import unittest
+
+class TestRepr(unittest.TestCase):
+    def test_(self):
+        x = Fraction(1, 2)
+
+        self.assertEqual(repr(x), "Fraction(1, 2)")
+
+class TestToString(unittest.TestCase):
+    def test_(self):
+        x = Fraction(1, 2)
+
+        self.assertEqual(str(x), "1/2")
+
+class TestToInt(unittest.TestCase):
+    def test_(self):
+        x = Fraction(10, 3)
+
+        self.assertEqual(int(x), 3)
+
+class TestToFloat(unittest.TestCase):
+    def test_(self):
+        x = Fraction(1, 2)
+
+        self.assertEqual(float(x), 0.5)
 
 class TestCommonFactor(unittest.TestCase):
     def test_ints(self):
         cd = Fraction._common_factor(2, -3, 5)
 
         self.assertEqual(30, cd)
+
+class TestAdd(unittest.TestCase):
+    def test_common(self):
+        x = Fraction(1, 4)
+        y = Fraction(2, 4)
+
+        z = x + y
+
+        self.assertEqual(z, 123)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
